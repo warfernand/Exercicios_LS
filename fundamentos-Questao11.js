b41ade74489684d5f83b9050dad11636895b5f7a
Iarlyson <Iarlyson.Santana@outlook.com>
2019-06-02 13:07:27 -0300

    Questão 11 add

---
+++ fundamentos-Questao11.js
@@ -0,0 +1,6 @@
+let numeros  =  Array ('10', '58', '68', '88', '25', '45');
+var numero = 3;
+var total = numeros.length;
+for(var p = numeros.length; p>=total-numero; p--){
+console.log(numeros[p]);
+}
